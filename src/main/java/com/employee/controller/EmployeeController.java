4201c5f21627546c31c19e7353ab07135da70481
nandinirakas <nandinirak@thelaunchclub.in>
2022-02-15 11:38:18 +0530

    Documentation

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import com.employee.service.EmployeeManagementImplVersion2;
 import com.employee.service.Validation;
 
 /**
- *  Get request from main and send response to service.
+ *  Get request from view and send response to service.
  */
 public class EmployeeController {
     
@@ -16,54 +16,112 @@ public class EmployeeController {
     private static final EmployeeManagementImplVersion2 EMPLOYEE_DATABASE = new EmployeeManagementImplVersion2();
     private static final Validation VALIDATION = new Validation();
 
+    /**
+     * Adding new employee details.
+     * 
+     * @param employee
+     */
     public static boolean addNewEmployee(final Employee employee) {
         return EMPLOYEE_DATABASE.addNewEmployee(employee);
         //EMPLOYEE_SERVICE.addNewEmployee(employee);
     }
 
+    /**
+     * For showing all employee details.
+     */
     public static Map<Integer, Employee> getEmployees() {
         return EMPLOYEE_DATABASE.getEmployees();
         //EMPLOYEE_SERVICE.getEmployees();
     }
 
+    /**
+     * Deleting employee detail by using id.
+     * 
+     * @param employeeId
+     */
     public static boolean deleteEmployee(final int employeeId) {
         return EMPLOYEE_DATABASE.deleteEmployee(employeeId);
         //EMPLOYEE_SERVICE.deleteEmployee(employeeId);
     }
     
+    /**
+     * Update employee detail by using id.
+     * 
+     * @param employee
+     */
     public static boolean updateEmployeeDetails(final Employee employee) {
         return EMPLOYEE_DATABASE.updateEmployeeDetails(employee);
         //EMPLOYEE_SERVICE.updateEmployeeDetails(employee);
     }
     
+    /**
+     * Validating given employee id.
+     * 
+     * @param employeeId
+     */
     public static boolean employeeIdValidation(final String employeeId) {
         return VALIDATION.employeeIdValidation(employeeId);
     }
     
+    /**
+     * Validating given employee name.
+     * 
+     * @param employeeName
+     */
     public static boolean employeeNameValidation(final String employeeName) {
         return VALIDATION.employeeNameValidation(employeeName);
     }
 
+    /**
+     * Validating given employee salary.
+     * 
+     * @param employeeSalary
+     */
     public static boolean employeeSalaryValidation(final String employeeSalary) {
         return VALIDATION.employeeSalaryValidation(employeeSalary);
     }
-       
+    
+    /**
+     * Validating given employee phone number.
+     * 
+     * @param phoneNumber
+     */
     public static boolean phoneNumberValidation(final String phoneNumber) {
         return VALIDATION.phoneNumberValidation(phoneNumber);
     }
    
+    /**
+     * Validating given employee joining date.
+     * 
+     * @param joiningDate
+     */
     public static boolean dateValidation(final String joiningDate) {
         return VALIDATION.dateValidation(joiningDate);
     }
     
+    /**
+     * Validating given employee choice given by user.
+     * 
+     * @param choice
+     */
     public static boolean validateChoice(final String choice) {
         return VALIDATION.validateChoice(choice);
     }
     
+    /**
+     * Checking whether employee id is already present for addition.
+     * 
+     * @param employeeId
+     */
     public static boolean checkEmployeeIdAdd(final int employeeId) {
         return EMPLOYEE_DATABASE.checkEmployeeIdAdd(employeeId);
     }
     
+    /**
+     * Checking whether employee id is present for updation.
+     * 
+     * @param employeeId
+     */
     public static boolean checkEmployeeIdUpdate(final int employeeId) {
         return EMPLOYEE_DATABASE.checkEmployeeIdUpdate(employeeId);
     }
--- src/main/java/com/employee/service/EmployeeManagementImplVersion2.java
+++ src/main/java/com/employee/service/EmployeeManagementImplVersion2.java
@@ -15,10 +15,16 @@ import com.employee.model.Employee;
 public class EmployeeManagementImplVersion2 implements EmployeeManagement {
     private static final EmployeeDao EMPLOYEE_DATABASE = new EmployeeDaoImpl();
     
+    /**
+     * Returns true if any employee is added.
+     */
     public boolean addNewEmployee(final Employee employee) {
         return EMPLOYEE_DATABASE.addNewEmployee(employee);
     }
     
+    /**
+     * Returns true if database is not empty.
+     */
     public Map<Integer, Employee> getEmployees() {
         final Map<Integer, Employee> employees = EMPLOYEE_DATABASE.getEmployees();
         
@@ -28,29 +34,45 @@ public class EmployeeManagementImplVersion2 implements EmployeeManagement {
         throw new NoRecordFoundException("No record found in database !");
     }
     
+    /**
+     * Returns true if deletion is done by checking id.
+     */
     public boolean deleteEmployee(final int employeeId) {
         
-        if(EMPLOYEE_DATABASE.deleteEmployee(employeeId)) {
+        if (EMPLOYEE_DATABASE.deleteEmployee(employeeId)) {
             return true;
         }
         throw new IdNotFoundException("Id not found!");
     }
     
+    /**
+     * Returns true if update is done by checking id.
+     */
     public boolean updateEmployeeDetails(final Employee employee) {
         return EMPLOYEE_DATABASE.updateEmployeeDetails(employee);
     }
     
+    /**
+     * Check whether employee id is present for addition.
+     * 
+     * @param employeeId
+     */
     public boolean checkEmployeeIdAdd(final int employeeId) {
         
-        if(!EMPLOYEE_DATABASE.getEmployees().containsKey(employeeId)) {
+        if (!EMPLOYEE_DATABASE.getEmployees().containsKey(employeeId)) {
             return true;
         }
         throw new IdAlreadyAvailableException("Id already found, enter new id");
     }
     
+    /**
+     * Check whether employee id is present for updation.
+     * 
+     * @param employeeId
+     */
     public boolean checkEmployeeIdUpdate(final int employeeId) {
         
-        if(EMPLOYEE_DATABASE.getEmployees().containsKey(employeeId)) {
+        if (EMPLOYEE_DATABASE.getEmployees().containsKey(employeeId)) {
             return true;
         }
         throw new IdNotFoundException("Id not found!");
